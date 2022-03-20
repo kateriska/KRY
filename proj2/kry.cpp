9d71aa91dd9fbaaa89db7d0717a75865e349a3c8
katerina <xforto00@vutbr.cz>
2022-03-20 21:31:33 +0100

    Start work with gmp lib

--- proj2/kry.cpp
+++ proj2/kry.cpp
@@ -6,30 +6,74 @@
 #include <bits/stdc++.h>
 #include <list>
 #include <vector>
-#include <gmp.h>
+#include <gmpxx.h>
+#include <getopt.h>
 
 using namespace std;
 
 int main (int argc, char **argv) {
+  int opt;
+  bool g = false;
+  int modulus_length;
+  //gmp_randstate_t state;
+  //mpz_t seed;
+  unsigned long int seed_value;
 
-    mpz_t a,b,c;
-    mpz_inits(a,b,c,NULL);
+  while ((opt = getopt (argc, argv, ":g:")) != -1)
+  {
+  switch (opt)
+  {
+    case 'g':
+      modulus_length = atoi(optarg);
+      cout << modulus_length << endl;
+      g = true;
+    break;
 
-    mpz_set_str(a, "1234555", 10);
-    mpz_set_str(b,"-5678", 10); //Decimal base
 
-    mpz_add(c,a,b);
 
-    cout<<"\nThe exact result is:";
-    mpz_out_str(stdout, 10, c); //Stream, numerical base, var
-    cout<<endl;
+    default:
+      cerr << "Error - Bad parametres!\n";
+      exit(EXIT_FAILURE);
+    break;
 
-    mpz_abs(c, c);
-    cout<<"The absolute value result is:";
-    mpz_out_str(stdout, 10, c);
-    cout<<endl;
+  }
+  }
 
-    cin.get();
+  // Create new random number generator state, and initialize state with the Mersenne Twister algorithm.
+  /*mpz_class ran;
+  gmp_randclass rr(gmp_randinit_default);
+  rr.seed(100000U);
+  ran =rr.get_z_bits(125);
+  long int random=ran.get_ui();*/
+
+  gmp_randclass r(gmp_randinit_default);
+  r.seed(time(NULL));
+  mpz_class random_number;
+  mpz_t msb_bit;
+  mpz_t random_number_value;
+  //mpz_randclass ran;
+//  gmp_randinit_mt(state);
+
+  // Seed random number generator.
+  //mpz_t seed;
+  //mpz_init_set_ui(seed, 100000U);
+  //gmp_randseed_ui(state, seed);
+  random_number = r.get_z_bits(modulus_length);
+  long int random = random_number.get_ui();
+  cout << random << endl;
+  //mpz_init(msb_bit);
+//  mpz_set_ui(msb_bit, modulus_length - 1);
+  //cout << mpz_get_ui(msb_bit) << endl;
+  mpz_set_ui(random_number_value, random);
+  mpz_setbit(random_number_value, modulus_length - 1);
+  cout << mpz_get_ui(random_number_value) << endl;
+
+  
+
+
+
+
+
 
     return 0;
 }
