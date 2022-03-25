ef254e3020edb1d72a2ba7bb501add467c562cdf
katerina <xforto00@vutbr.cz>
2022-03-25 14:04:20 +0100

    Start work on miller rabin

--- proj2/kry.cpp
+++ proj2/kry.cpp
@@ -11,6 +11,80 @@
 
 using namespace std;
 
+bool miller_rabin_test(mpz_t n)
+{
+  mpz_t two_value;
+  mpz_t zero_value;
+  mpz_t one_value;
+  mpz_init(two_value);
+  mpz_init(zero_value);
+  mpz_init(one_value);
+  mpz_set_str(two_value, "2", 10);
+  mpz_set_str(zero_value, "0", 10);
+  mpz_set_str(one_value, "1", 10);
+  mpz_t modulo_result;
+  mpz_init(modulo_result);
+  mpz_mod(modulo_result, n, two_value);
+
+  if (mpz_cmp(n, two_value) == 0)
+  {
+    return true;
+  }
+  else if (mpz_cmp(modulo_result, zero_value))
+  {
+    cout << "HHHHHHH " << endl;
+    return false;
+  }
+
+  mpz_t n1;
+  mpz_init(n1);
+  mpz_t k;
+  mpz_init(k);
+  mpz_t u;
+  mpz_init(u);
+  mpz_set_str(k, "0", 10);
+  mpz_sub(n1, n, one_value);
+  mpz_set(u, n1);
+
+  mpz_mod(modulo_result, u, two_value);
+  /*
+  while (mpz_cmp(modulo_result, zero_value))
+  {
+
+  }*/
+
+
+
+
+
+  /*
+  cout << "GGGG" << endl;
+  mpz_t two_value;
+  mpz_set_str(two_value, "2", 10);
+  mpz_t modulo_result;
+  //mpz_init(modulo_result,NULL);
+  cout << mpz_mod(modulo_result, n, two_value) << endl;
+
+  if (mpz_cmp(n, two_value) == 0)
+  {
+    return true;
+  }
+
+  else if (mpz_mod(modulo_result, n, two_value) == true)
+  {
+    cout << "HHHHHHH " << endl;
+    return false;
+  }
+  //mpz_t n1;
+  //mpz_set_ui(n1, n_value - 1);
+  //cout << mpz_get_ui(n1) << endl;*/
+  return true;
+
+
+
+
+}
+
 int main (int argc, char **argv) {
   int opt;
   bool g = false;
@@ -64,11 +138,20 @@ int main (int argc, char **argv) {
   //mpz_init(msb_bit);
 //  mpz_set_ui(msb_bit, modulus_length - 1);
   //cout << mpz_get_ui(msb_bit) << endl;
-  mpz_set_ui(random_number_value, random);
-  mpz_setbit(random_number_value, modulus_length - 1);
-  cout << mpz_get_ui(random_number_value) << endl;
 
-  
+  mpz_init(random_number_value);
+  mpz_set_ui(random_number_value, random);
+  cout << "FFFF" << endl;
+  mpz_setbit(random_number_value, modulus_length - 1);
+  cout << "FFFF" << endl;
+  cout << mpz_get_ui(random_number_value) << endl;
+  //cout << "FFFF" << endl;
+  miller_rabin_test(random_number_value);
+
+
+
+
+
 
 
 
