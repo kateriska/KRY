4a5961e5da71f7b0d68513d11588cb1e4dab2366
katerina <xforto00@vutbr.cz>
2022-04-04 15:01:45 +0200

    Functional but ultra slow after 32 b

--- proj2/kry.cpp
+++ proj2/kry.cpp
@@ -12,7 +12,7 @@
 using namespace std;
 
 unsigned long int modular_exponent(mpz_t a, mpz_t u, mpz_t n);
-bool find_witness(mpz_t a, mpz_t u, mpz_t n, mpz_t n1);
+bool find_witness(mpz_t a, mpz_t u, mpz_t n, mpz_t n1, mpz_t k);
 
 bool miller_rabin_test(mpz_t n)
 {
@@ -78,7 +78,7 @@ bool miller_rabin_test(mpz_t n)
   }
 }
  // ok nahoru
-  /*
+
   mpz_t left_side;
   mpz_t right_side;
   mpz_init(left_side);
@@ -89,15 +89,17 @@ bool miller_rabin_test(mpz_t n)
   cout << k_int << endl;
 
   mpz_t right_side_power;
+  mpz_init(right_side_power);
   mpz_pow_ui(right_side_power, two_value, k_int);
   mpz_mul(right_side, right_side_power, u);
 
   unsigned long int left_side_int = mpz_get_ui(left_side);
   unsigned long int right_side_int = mpz_get_ui(right_side);
 
+  cout << "Left and right side " << endl;
   cout << right_side_int << endl;
   cout << left_side_int << endl;
-  */
+
 
   mpz_t num1;
   mpz_init(num1);
@@ -110,74 +112,92 @@ bool miller_rabin_test(mpz_t n)
   mpz_set_str(num2, "2", 10);
   mpz_set_str(num3, "2", 10);
 
-  cout << modular_exponent(num1, num2, num3) << endl;
+  //cout << modular_exponent(num1, num2, num3) << endl;
 
   unsigned long int generated_a_range = mpz_get_ui(n) - 2 + 1;
   cout << "LLLL" << endl;
   cout << generated_a_range << endl;
 
   srand(time(0));
-  unsigned long int a_int = rand() % generated_a_range + 2;
-  cout << a_int << endl;
+  //unsigned long int a_int = rand() % generated_a_range + 2;
+  //cout << a_int << endl;
 
   // ok nahoru
-  /*
-  mpz_t a;
-  mpz_init(a);
-  mpz_set_ui(a, a_int);
+
+  //mpz_t a;
+  //mpz_init(a);
+  //mpz_set_ui(a, a_int);
 
 
 
 
 
 
-  for (int i = 0; i < 100; i++)
+
+  for (int i = 0; i < 5; i++)
   {
-    if (find_witness(a, u, n, n1))
+    unsigned long int a_int = rand() % generated_a_range + 2;
+    cout << a_int << endl;
+    mpz_t a;
+    mpz_init(a);
+    mpz_set_ui(a, a_int);
+    if (find_witness(a, u, n, n1, k))
     {
+      cout << "Witness that p is not prime " << endl;
       return false;
     }
   }
-  */
-
-
-
 
   return true;
 }
-/*
-bool find_witness(mpz_t a, mpz_t u, mpz_t n, mpz_t n1)
+
+bool find_witness(mpz_t a, mpz_t u, mpz_t n, mpz_t n1, mpz_t k)
 {
   mpz_t two_value;
   mpz_init(two_value);
   mpz_set_str(two_value, "2", 10);
 
+  mpz_t three_value;
+  mpz_init(three_value);
+  mpz_set_str(three_value, "3", 10);
+
+  cout << "===PARAMS===" << endl;
+  cout << mpz_get_ui(a) << endl;
+  cout << mpz_get_ui(u) << endl;
+  cout << mpz_get_ui(n) << endl;
   unsigned long int modular_exponent_result = modular_exponent(a, u, n);
-  cout << "Modular exponent result " << modular_exponent_result << endl;;
+  cout << "Modular exponent result " << modular_exponent_result << endl;
 
 
-
+  unsigned long int n_int = mpz_get_ui(n);
   if (modular_exponent_result == 1)
   {
     return false;
   }
 
+  unsigned long int k_int = mpz_get_ui(k);
+  cout << "K " << k_int << endl;
 
-  for (unsigned long int i = 0; i < mpz_get_ui(u); i++)
+  for (unsigned long int i = 0; i < k_int; i++)
   {
     cout << "I " << i << endl;
-    modular_exponent_result = modular_exponent(a, u, n);
+  //  modular_exponent_result = modular_exponent(a, u, n);
 
     mpz_t modular_exponent_multiply;
     mpz_t modular_exponent_pow;
     mpz_init(modular_exponent_multiply);
     mpz_init(modular_exponent_pow);
     mpz_pow_ui(modular_exponent_pow, two_value, i);
+    cout << "Modular exponent power" << endl;
+    cout << mpz_get_ui(modular_exponent_pow) << endl;
     mpz_mul(modular_exponent_multiply, modular_exponent_pow, u);
-    modular_exponent_result = modular_exponent(a, modular_exponent_multiply, n);
+    cout << "Modular exponent multiply" << endl;
+    cout << mpz_get_ui(modular_exponent_multiply) << endl;
+    unsigned long int modular_exponent_result_iteration = modular_exponent(a, modular_exponent_multiply, n);
 
     cout << "N1 " <<  mpz_get_ui(n1) << endl;
-    if (modular_exponent_result == mpz_get_ui(n1))
+    cout << "New modular exponent result " << modular_exponent_result_iteration << endl;
+    if (modular_exponent_result_iteration == mpz_get_ui(n1))
     {
       return false;
     }
@@ -187,7 +207,43 @@ bool find_witness(mpz_t a, mpz_t u, mpz_t n, mpz_t n1)
 
   return true;
 }
+
+// https://www.geeksforgeeks.org/count-total-bits-number/
+unsigned int countBits(unsigned int n)
+{
+   unsigned int count = 0;
+   while (n)
+   {
+        count++;
+        n >>= 1;
+    }
+    return count;
+}
+// https://www.geeksforgeeks.org/binary-representation-of-a-given-number/
+/*
+string bin(unsigned int n)
+{
+    if (n > 1)
+        bin(n >> 1);
+
+    printf("%d", n & 1);
+    int res = n & 1;
+    cout << "Counting res " << endl;
+    cout << res << endl;
+    cout << "Counting res endl" << endl;
+    string res_str = to_string(res);
+    return res_str;
+}
 */
+// https://stackoverflow.com/questions/22746429/c-decimal-to-binary-converting
+string toBinary(unsigned long int n) {
+    if (n==0) return "0";
+    else if (n==1) return "1";
+    else if (n%2 == 0) return toBinary(n/2) + "0";
+    else if (n%2 != 0) return toBinary(n/2) + "1";
+}
+
+
 
 unsigned long int modular_exponent(mpz_t a, mpz_t u, mpz_t n)
 {
@@ -203,8 +259,31 @@ unsigned long int modular_exponent(mpz_t a, mpz_t u, mpz_t n)
   mpz_init(result);
   mpz_set_str(result, "1", 10);
 
+  unsigned long int u_int = mpz_get_ui(u);
+
+
+  cout << "Var " << countBits(u_int) <<  endl;
+  string u_bin = toBinary(u_int);
+
+  for (int i = 0; i < countBits(u_int); i++)
+  {
+    cout << "HHHHHHHHHHHH" << endl;
+    cout << u_bin[i] << endl;
+    cout << "FFFFFFFFFFFF" << endl;
+
+    mpz_pow_ui(result, result, 2);
+    if (u_bin[i] == '1')
+    {
+      mpz_mul(result, result, a);
+    }
+    mpz_mod(result, result, n);
+
+
+  }
+
   //mpz_mod(a, a, n);
 
+  /*
   if (mpz_cmp(u, zero_value) > 0) {
   while (true)
   {
@@ -238,7 +317,7 @@ unsigned long int modular_exponent(mpz_t a, mpz_t u, mpz_t n)
     }
   }
 }
-
+*/
 return mpz_get_ui(result);
 
 
@@ -304,12 +383,44 @@ int main (int argc, char **argv) {
 
   mpz_init(random_number_value);
   mpz_set_ui(random_number_value, random);
-  cout << "FFFF" << endl;
   mpz_setbit(random_number_value, modulus_length - 1);
-  cout << "FFFF" << endl;
+  cout << "Generated number " << endl;
   cout << mpz_get_ui(random_number_value) << endl;
-  //cout << "FFFF" << endl;
-  miller_rabin_test(random_number_value);
+  cout << "FFFF" << endl;
+
+  mpz_t prime;
+  mpz_init(prime);
+  mpz_set_str(prime, "25", 10);
+
+  int generated_prime_count = 2;
+  mpz_t one_value;
+  mpz_init(one_value);
+  mpz_set_str(one_value, "1", 10);
+
+  if (miller_rabin_test(random_number_value))
+  {
+  cout << mpz_get_ui(random_number_value) << " is potential prime" << endl;
+}
+
+  vector <unsigned long int> potential_primes;
+  while (generated_prime_count > 0)
+  {
+  if (miller_rabin_test(random_number_value))
+  {
+    cout << mpz_get_ui(random_number_value) << " is potential prime" << endl;
+    generated_prime_count--;
+    unsigned long int random_number_value_int = mpz_get_ui(random_number_value);
+    potential_primes.push_back(random_number_value_int);
+
+  }
+  mpz_add(random_number_value, random_number_value, one_value);
+  }
+
+  cout << "Prime numms:" << endl;
+  cout << potential_primes.at(0) << endl;
+  cout << potential_primes.at(1) << endl;
+
+
 
 
 
